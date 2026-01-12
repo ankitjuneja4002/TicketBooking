55036ba7a2c1955af0b874f3bca1b4d50a8b02c9
Ankit Juneja <ankitjuneja4002@gmail.com>
2026-01-13 01:07:26 +0530

    Add files via upload

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,13 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookingService {
+
+    private User user;
+
+    public UserBookingService(User user) {
+        this.user = user;
+
+    }
+}
